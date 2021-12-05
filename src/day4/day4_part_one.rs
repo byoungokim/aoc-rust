932928d4df50eaea691db6370ad090c9efabbe53
Byoungo Kim <bo.kim@greenlabs.co.kr>
2021-12-06 00:37:25 +0900

    implement day4 solution  - implement day4 solution, create a simple function and use it to validate the winning condition
    Signed-off-by: Byoungo Kim <bo.kim@greenlabs.co.kr>

---
+++ src/day4/day4_part_one.rs
@@ -0,0 +1,77 @@
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
+
+fn analyze_bingo(sequence: Vec<i32> , bingo: Vec<i32>) -> (i32, i32){
+    let mut sum = 0;
+    for i in 0..bingo.len() {
+        sum += bingo[i];
+    }
+    let mut rows = [0_u8; 5];
+    let mut cols = [0_u8; 5];
+    let mut n_win = 0;
+    'outer: for i in 0..sequence.len() {
+        for j in 0..bingo.len() {
+            if bingo[j] == sequence[i] {
+                print!("{}, ", bingo[j]);
+                sum-= bingo[j];
+                rows[j/5]+=1;
+                if rows[j/5] == 5 {
+                    n_win = i as i32;
+                    break 'outer;
+                }
+                cols[j%5]+=1;
+                if cols[j%5] == 5 {
+                    n_win = i as i32;
+                    break 'outer;
+                }
+            }
+        }
+    }
+    return (n_win, sum);
+}
+
+fn main() -> io::Result<()> {
+    let f = File::open("day4_part1.txt")?;
+    let mut buf = BufReader::new(f);
+
+    let mut input = String::new();
+    buf.read_line(&mut input)?;
+    let sequence : Vec<&str> = input.split(',').collect();
+    let mut sequence_int: Vec<i32> = Vec::new();
+    sequence.iter().for_each(|x| {
+        if let Ok(value) = x.parse::<i32>() {
+            println!("{} {}", x, x.len());
+            sequence_int.push(value);
+        }
+    });
+    println!("sequence.len {}", sequence.len());
+    let mut min_seq = sequence.len() as i32;
+    let mut score = 0;
+    let mut bingo: Vec<i32> = Vec::new();
+
+    for line in buf.lines() {
+        if let Ok(line_str) = line {
+            let temp : Vec<&str> = line_str.split(' ').collect();
+            for element in temp {
+                if element.len() > 0 {
+                    bingo.push(element.parse::<i32>().unwrap());
+                }
+            }
+            println!("len: {}, line_str: {}", bingo.len(), line_str);
+            if bingo.len() == 25 {
+                let (n_seq, sum) = analyze_bingo(sequence_int.clone(), bingo.clone());
+                if n_seq < min_seq {
+                    min_seq = n_seq;
+                    score = sum * sequence_int[(min_seq) as usize];
+                }
+                bingo.clear();
+            }
+            println!("min_seq: {}, score: {}", min_seq, score);
+        }
+    }
+
+    Ok(())
+
+    // println!("n_passwords: {}", sequence.len());
+}
